d5d3e7a71b0f4a4474407b74b353f3d9ac024178
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-03-17 07:18:21 -0700

    practice nested loop task 2

---
+++ Lectures/Lecture nested loop/practiceLoop2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+	
+    for (int i = 0; i < 5 ; i++){
+        for(int j = 1 ; j <= 5 ; j++){
+    cout << i + j << " ";
+        }
+        cout << endl ;
+    }
+	return 0;
+}
