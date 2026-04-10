80a871f69716910b50819a3e02efa44901ad561e
DESKTOP-SJO43KC\Lenovo <abdulrehmancsf25@gmail.com>
2026-04-09 22:39:27 -0700

    Pattern for printing ALPHABETS in decrasing increasing order

--- Practice mids/patterns.cpp
+++ Practice mids/patterns.cpp
@@ -25,7 +25,42 @@ cout << endl ;
         cout << endl ;
     }
         */
+            /*
+ ABCD        
+A B C D     
+A  B  C  D                 DRAW THE PATTERN 
+A   B   C   D
+Z   Y   X   W
+Z  Y  X  W
+Z Y X W
+ZYXW     */
+       int n ;
+       cin >> n ;
+       for(int i = 0 ; i < n*2 ; i++){
+    char first = 'A' , second = 'Z' ;
+          for(int j = 0 ; j < n ; j++){
+            
+if(i < n ){
+    int space = i ;
+     cout << first  ;
+     while(space){
+        cout << " " ;
+        space -- ;
+     }
+     first += 1 ;
+}
+else {
+      int space = (n*2) - i - 1 ;
+      cout << second ;
+      while(space){
+        cout << " " ;
+        space -- ;
+      }
+      second -= 1;
+}
+          }
+   cout << endl ;
 
-       
+       }
     return 0 ;
 }
